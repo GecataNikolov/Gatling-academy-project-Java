00ffdf3c39f852eab1172d998d2f2e70af7512cb
Georgi Nikolov <georgi.nikolov@techpods.co>
2023-03-28 17:28:50 +0300

    Add before method to print the Simulatio defaults options
    * Before method added in DemoStoreSimulation printing the user count, ramp and test duration in the console before execution

--- src/test/java/gatlingDemoStore/DemoStoreSimulation.java
+++ src/test/java/gatlingDemoStore/DemoStoreSimulation.java
@@ -1,13 +1,19 @@
 package gatlingDemoStore;
 
-import static io.gatling.javaapi.core.CoreDsl.*;
-
 import gatlingDemoStore.helpers.SimulationDefaults;
 import gatlingDemoStore.pageObject.UserJourneys;
-import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.core.Simulation;
+
+import static io.gatling.javaapi.core.CoreDsl.rampUsers;
 
 
 public class DemoStoreSimulation extends Simulation {
+    @Override
+    public void before() {
+        System.out.printf("Running test with %d users%n", SimulationDefaults.USER_COUNT);
+        System.out.printf("Ramping users over  %d seconds%n", SimulationDefaults.RAMP_DURATION.getSeconds());
+        System.out.printf("Total test duration: %d%n", SimulationDefaults.TEST_DURATION.getSeconds());
+    }
 
     {
         setUp(
